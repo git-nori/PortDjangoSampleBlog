95d4beb68c0757e76f565b9a955b2a94e8eaa820
HironoriSato <maza.hs5360@gmail.com>
2019-07-12 03:45:20 +0900

    [add]ホーム画面を新規作成

--- sampleblog/views.py
+++ sampleblog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Article
 
 # Create your views here.
+def home(request):
+    articles = Article.objects.all().order_by('-updated_at')
+
+    return render(request, 'sampleblog/home.html', {'articles': articles})
