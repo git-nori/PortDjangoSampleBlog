1965ab9cdd453bf8c295ef649d8cb78679550002
HironoriSato <maza.hs5360@gmail.com>
2019-07-12 20:15:39 +0900

    [add]記事詳細画面を新規作成

--- sampleblog/views.py
+++ sampleblog/views.py
@@ -7,3 +7,10 @@ def home(request):
     articles = Article.objects.all().order_by('-updated_at')
 
     return render(request, 'sampleblog/home.html', {'articles': articles})
+
+
+def article_detail(request, pk):
+    """記事詳細画面"""
+    article = get_object_or_404(Article, pk=pk)
+
+    return render(request, 'sampleblog/article_detail.html', {'article': article})
