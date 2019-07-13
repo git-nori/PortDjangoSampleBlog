32afbc1417d759cad0387f2cae3de77b58cbf3d0
HironoriSato <maza.hs5360@gmail.com>
2019-07-13 13:03:58 +0900

    [add]記事編集画面を新規作成

--- sampleblog/urls.py
+++ sampleblog/urls.py
@@ -6,4 +6,5 @@ app_name = 'sampleblog'
 urlpatterns = [
     path('', views.home, name='home'),  # ホーム画面(記事一覧)
     path('article_detail/<int:pk>/', views.article_detail, name='article_detail'),  # 記事詳細画面
+    path('article_edit/<int:pk>/', views.article_edit, name='article_edit'),  # 記事編集画面
 ]
