19feabbf6a3f337b7b17be93e0ebf6d345b22fd8
HironoriSato <maza.hs5360@gmail.com>
2019-07-15 23:23:44 +0900

    [add]ユーザー詳細画面を新規作成

--- sampleblog/urls.py
+++ sampleblog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('article_edit/<int:pk>/', views.article_edit, name='article_edit'),  # 記事編集画面
     path('article_add/', views.article_add, name='article_add'),  # 記事追加画面
     path('user_list', views.user_list, name='user_list'),  # ユーザー一覧画面
+    path('user_detail/<int:pk>/', views.user_detail, name='user_detail'),  # ユーザー詳細画面
 ]
