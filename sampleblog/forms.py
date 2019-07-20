a9cc26bfe9c33d6bee504525f2227819a2d8b65f
HironoriSato <maza.hs5360@gmail.com>
2019-07-20 14:23:07 +0900

    [mod]ユーザー変更フォームを追加

--- sampleblog/forms.py
+++ sampleblog/forms.py
@@ -1,9 +1,16 @@
 from django.forms import ModelForm
-from .models import Article
+from .models import Article, CustomUser
+
+
+class CustomUserUpdateForm(ModelForm):
+    class Meta:
+        """ユーザー変更のフォーム"""
+        model = CustomUser
+        fields = ('username', 'gender', 'image')
 
 
 class ArticleForm(ModelForm):
     class Meta:
         """記事のフォーム"""
         model = Article
-        fields = ('title', 'content', 'user', )
+        fields = ('title', 'content', 'user',)
