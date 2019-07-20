653efc47bc3179235300f0aae8183a59617ad43f
HironoriSato <maza.hs5360@gmail.com>
2019-07-20 16:07:21 +0900

    [mod]ユーザー登録フォームを追加

--- sampleblog/forms.py
+++ sampleblog/forms.py
@@ -1,5 +1,13 @@
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
 from .models import Article, CustomUser
+
+
+class CustomUserCreateForm(UserCreationForm):
+    class Meta:
+        """ユーザー作成フォーム"""
+        model = CustomUser
+        fields = ('username', 'password1', 'password2')
 
 
 class CustomUserUpdateForm(ModelForm):
